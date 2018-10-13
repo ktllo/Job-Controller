c71169357db71d6ea6dd6b78fcb2de0706a86c75
LO Kam Tao Leo <leolo@leolo.org>
2018-10-13 14:29:45 +0800

    Adding test suit

---
+++ test/org/leolo/util/jobcontrol/test/Test.java
@@ -0,0 +1,18 @@
+package org.leolo.util.jobcontrol.test;
+
+import static org.junit.Assert.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+	
+	
+	Logger log = LoggerFactory.getLogger(Test.class);
+	@org.junit.Test
+	public void test() {
+		log.info("test");
+//		fail("Not yet implemented");
+	}
+
+}
